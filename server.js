66170da58c6c214e8210bd320864ddc7ed8461e4
guillee-sg <guillermo.salvan@hotmail.com>
2026-05-16 22:14:29 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,31 @@
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
+const app = express();
+
+/*
+  Dynmap proxy
+*/
+
+app.use(
+  '/map',
+  createProxyMiddleware({
+    target: 'http://clasetercerob.falix.gg:8123',
+    changeOrigin: true,
+    ws: true
+  })
+);
+
+/*
+  Homepage
+*/
+
+app.get('/', (req, res) => {
+  res.send('Dynmap proxy online');
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Listening on ${PORT}`);
+});
